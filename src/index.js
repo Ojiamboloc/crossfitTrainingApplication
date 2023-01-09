c6855555dfb305ec034e351ad10b3ee988aa1e0b
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-08 22:05:01 -0800

    hooking up our workout router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const v1Router = require("./v1/routes");
+//const v1Router = require("./v1/routes");
+const v1WorkoutRouter=require ("./v1/routes/workoutRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +9,8 @@ const PORT = process.env.PORT || 3000;
 /*app.get("/", (req, res, next) =>{
     res.send("<h2>Working</h2>");
 });*/
-app.use("/api/v1", v1Router);
+//app.use("/api/v1", v1Router);
+app.use("/api/v1/workouts", v1WorkoutRouter);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
